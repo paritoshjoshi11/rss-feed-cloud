590d4aab2e07aaeeb4069f473c802afde90eace9
paritosh <paritoshjoshi11@gmail.com>
2024-11-09 13:05:48 +0530

    update consumer to push data again to mongo db with spark stream

--- consumer.py
+++ consumer.py
@@ -1,25 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
+from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, ArrayType, StringType
 from pyspark.sql.functions import from_json, udf
 import google.generativeai as genai
 from dotenv import load_dotenv
+import os
 # Load environment variables from .env file
 load_dotenv()
 
+
+
 kafka_config = os.environ.get('SPARK_KAFKA_CONFIG')
 kafka_server = os.environ.get('SPARK_KAFKA_CLUSTER')
+mongodb_uri= os.environ.get('NONGO_URI')
+
+# Initialize Spark session with MongoDB connector
+spark = SparkSession.builder \
+    .appName("MongoDB Write Example") \
+    .config("spark.mongodb.input.uri", mongodb_uri) \
+    .config("spark.mongodb.output.uri", mongodb_uri) \
+    .getOrCreate()
+
 df = (spark
-  .read
+  .readStream
   .format("kafka")
-  .option("kafka.bootstrap.servers", "kafka_server")
+  .option("kafka.bootstrap.servers", kafka_server)
   .option("subscribe", "topic_0")
-  .option("startingOffsets", "earliest")
-  .option("endingOffsets", "latest")
+  .option("startingOffsets", "latest")
   .option("kafka.security.protocol","SASL_SSL") \
   .option("kafka.sasl.mechanism", "PLAIN") \
-  .option("kafka.sasl.jaas.config", kafka_config) \
+  .option("kafka.sasl.jaas.config",kafka_config) \
   .load()
+  .limit(5)
 )
 schema = StructType([
     StructField("title", StringType(), True),
@@ -33,15 +46,6 @@ json_df = (df
            .selectExpr("CAST(value AS STRING) as json")
            .select(from_json("json", schema).alias("data"))
            .select("data.*"))
-
-# Display the structured data in the console for testing
-#display(json_df)
-
-def summarise_desc(description_text):
-    genai.configure(api_key='')
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(f"Give summary on {description_text} in 100 words")
-    return response
 
 def parse_medium_story_text(link):
     new_link = "https://freedium.cfd/"+link
@@ -58,11 +62,23 @@ def parse_medium_story_text(link):
 parse_medium_story_text_udf = udf(parse_medium_story_text, StringType())
 link_column = json_df['link']
 json_df_with_desc = json_df.withColumn("description", parse_medium_story_text_udf(link_column))
-
-# summarise_description_udf = udf(summarise_desc, StringType())
-# desc_column = json_df_with_desc['description']
-# json_df_with_desc_sum = json_df_with_desc.withColumn("summary", summarise_description_udf(desc_column))
-
-# Print the received events to the console
+# print("Printing the extracted info with blog details ")
 display(json_df_with_desc.limit(10))
-# display(json_df_with_desc_sum.limit(10))
+
+# MongoDB configuration
+collection_name = "feed_life_desc"
+
+dataStreamWriter = json_df_with_desc.writeStream \
+        .format("mongodb") \
+        .option("checkpointLocation", "checkpoint")\
+        .option("spark.mongodb.connection.uri", mongodb_uri) \
+        .option("spark.mongodb.database", "rss_feed") \
+        .option("spark.mongodb.collection", collection_name) \
+        .outputMode("append")
+
+# Start the MongoDB query
+query_mongodb = dataStreamWriter.start()
+
+# Await termination for both queries
+query_mongodb.awaitTermination()
+
